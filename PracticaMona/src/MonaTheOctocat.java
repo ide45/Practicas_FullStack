ff5ffc39d3ca2c1030b35ffec7c3b2f400e28b16
Imatiuh_islas <91546625+ide45@users.noreply.github.com>
2022-04-27 20:34:03 -0500

    Add files via upload

---
+++ PracticaMona/src/MonaTheOctocat.java
@@ -0,0 +1,96 @@
+package practicaMona;
+
+public class MonaTheOctocat {
+    private String profesion;
+
+    public MonaTheOctocat(String profesion){
+        this.profesion = profesion;
+    }
+
+    public void makeAction(String accion){
+        System.out.println("---" + this.profesion.toUpperCase() + "---");
+        System.out.println("Miren, la mona " + this.profesion + " esta " + accion);
+    }
+
+    public void loQueDiceLaMona(){
+        System.out.println("-Hola, soy una mona bonis");
+    }
+}
+
+class monaMecanica extends MonaTheOctocat{
+    public monaMecanica(String nombre) { super(nombre); }
+
+    public void makeAction(String accion) {
+        super.makeAction(accion);
+    }
+
+    @Override
+    public void loQueDiceLaMona() {
+        System.out.println("-Estoy a punto de descrubir un motor que se mueve con agua!");
+    }
+}
+
+class monaAviadora extends MonaTheOctocat{
+    public monaAviadora(String nombre) { super(nombre); }
+
+    public void makeAction(String accion) {
+        super.makeAction(accion);
+    }
+
+    @Override
+    public void loQueDiceLaMona() {
+        System.out.println("-Shiuuuum ✈︎");
+    }
+}
+
+class monaLuchadora extends MonaTheOctocat{
+    public monaLuchadora(String nombre) { super(nombre); }
+
+    public void makeAction(String accion) {
+        super.makeAction(accion);
+    }
+
+    @Override
+    public void loQueDiceLaMona() {
+        System.out.println("-No subestimes mi fuerza, fuaaa!");
+    }
+}
+
+class monaCientifica extends MonaTheOctocat{
+    public monaCientifica(String nombre) { super(nombre); }
+
+    public void makeAction(String accion) {
+        super.makeAction(accion);
+    }
+
+    @Override
+    public void loQueDiceLaMona() {
+        System.out.println("-He descubierto algo que dejará sin palabras al mundo entero!");
+    }
+}
+
+class monaActriz extends MonaTheOctocat{
+    public monaActriz(String nombre) { super(nombre); }
+
+    public void makeAction(String accion) {
+        super.makeAction(accion);
+    }
+
+    @Override
+    public void loQueDiceLaMona() {
+        System.out.println("-Tengo tantos óscares, ya ni sé que hacer con ellos...");
+    }
+}
+
+class monaProgramadora extends MonaTheOctocat{
+    public monaProgramadora(String nombre) { super(nombre); }
+
+    public void makeAction(String accion) {
+        super.makeAction(accion);
+    }
+
+    @Override
+    public void loQueDiceLaMona() {
+        System.out.println("-Sé hacer cualquier cosa pero... centrar un div, eso es maligno");
+    }
+}
---
+++ PracticaMona/src/imprimeMonas.java
@@ -0,0 +1,34 @@
+package practicaMona;
+
+public class imprimeMonas {
+    public static void main(String[] args) {
+        muestraMonas();
+    }
+
+    private static void muestraMonas(){
+        monaMecanica mona = new monaMecanica("mecánica");
+        monaAviadora mona1 = new monaAviadora("aviadora");
+        monaLuchadora mona2 = new monaLuchadora("luchadora");
+        monaCientifica mona3 = new monaCientifica("cientifica");
+        monaActriz mona4 = new monaActriz("actriz");
+        monaProgramadora mona5 = new monaProgramadora("programadora");
+
+        mona.makeAction("probando los nuevos ajustes que hizo a su deportivo!");
+        mona.loQueDiceLaMona();
+
+        mona1.makeAction("dentro de un boing o mejor dicho... un boeing!");
+        mona1.loQueDiceLaMona();
+
+        mona2.makeAction("golpeando a su contrincante...auch!");
+        mona2.loQueDiceLaMona();
+
+        mona3.makeAction("resolviendo problemas muy complejos!");
+        mona3.loQueDiceLaMona();
+
+        mona4.makeAction("rodando una nueva pelicula!");
+        mona4.loQueDiceLaMona();
+
+        mona5.makeAction("implementando un objeto!");
+        mona5.loQueDiceLaMona();
+    }
+}
